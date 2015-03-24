7be60aa314f75838f364fb9e49ffa2e447267f26
Jatin <jatinx.lol@gmail.com>
2015-03-25 00:21:24 +0530

    Update example.php

--- example.php
+++ example.php
@@ -2,8 +2,10 @@
    require_once dirname(__FILE__) . "/class.countify.php";
    $countify = new Countify;
    echo $countify->instagram_followers("instagram");  // Instagram Username ~ Return Followers Count
+   /*
    echo $countify->instagram_likes("https://instagram.com/p/0GxfObTYMz/");  // Instagram Image URL ~ Return Likes Count
    echo $countify->twitter_followers("twitter");  // Twitter Username ~ Return Followers Count
    echo $countify->twitter_retweet("https://twitter.com/twitter/status/532235053883858944");   // Twitter Status URL ~ Return Retweets Count 
    echo $countify->twitter_fav("https://twitter.com/twitter/status/532235053883858944");   // Twitter Status URL ~ Return Fav Count
+   */
 ?>
