238ba3fbd782e1ed0c8a7e398e7ae27a20fe3f5a
Jatin <jatinx.lol@gmail.com>
2015-03-25 00:07:40 +0530

    Create class.countify.php

---
+++ class.countify.php
@@ -0,0 +1,55 @@
+<?php
+/* Class Started */
+class Countify {
+    public static function instagram_followers($username) {
+        $file = @file_get_contents('http://instagram.com/' . $username);
+        preg_match('/\"followed_by":(.*?)\,/', $file, $mfc);
+        if ($mfc[1]) {
+            return $mfc[1];
+        } 
+        else {
+            return "Invaid Instagram Username";
+        }
+    }
+    public static function instagram_likes($url) {
+        $file = @file_get_contents($url);
+        preg_match('/\"count":(.*?)\,/', $file, $mfc);
+        if ($mfc[1]) {
+            return $mfc[1];
+        } 
+        else {
+            return "Invaid Image URL";
+        }
+    }
+    public static function twitter_followers($username) {
+        $file = @file_get_contents("https://twitter.com/" . $username);
+        preg_match('/\"followers_count":(.*?)\,/', htmlspecialchars_decode($file), $mfc);
+        if ($mfc[1]) {
+            return $mfc[1];
+        } 
+        else {
+            return "Invaid Twitter Username";
+        }
+    }
+    public static function twitter_retweet($url) {
+        $file = @file_get_contents($url);
+        preg_match_all('/<strong>(.*?)<\/strong>/is', $file, $mfc);
+        if ($mfc[1][0]) {
+            return $mfc[1][0];
+        } 
+        else {
+            return "Invaid Twitter Status";
+        }
+    }
+    public static function twitter_fav($url) {
+        $file = @file_get_contents($url);
+        preg_match_all('/<strong>(.*?)<\/strong>/is', $file, $mfc);
+        if ($mfc[1][1]) {
+            return $mfc[1][1];
+        } 
+        else {
+            return "Invaid Twitter Status";
+        }
+    }
+}
+?>
